7e299775897dfdf226a3494eab646495cc057df5
arbhassan <hassanjawad1321@gmail.com>
2021-08-29 15:14:20 +0500

    added * to allowed hosts

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'km6iv=q!fm&og!dcku!!aohf&dyk4bn==#*ayxe#x$_90(@+1v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
