7ffcedd567c5b75a0337da5b20553b2eb85512dd
arbhassan <hassanjawad1321@gmail.com>
2021-09-04 00:28:27 +0500

    Added and used context in templates

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Lead
 # Create your views here.
 
 def home_page(request):
-    # return HttpResponse("Hello World!")
-    return render(request, "second_page.html")
+    leads = Lead.objects.all() 
+    context = {
+        "leads": leads
+    }
+
+    return render(request, "second_page.html", context)
