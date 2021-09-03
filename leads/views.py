48bc4f794b16013fffe285ff9364c9b6b3385651
arbhassan <hassanjawad1321@gmail.com>
2021-09-04 00:14:02 +0500

    Added our first Template

--- leads/views.py
+++ leads/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+    return render(request, "second_page.html")
